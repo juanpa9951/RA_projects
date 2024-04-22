46c1e6f8b1d1981ba251b0a5a13504d7e6795fb9
Juan Pablo Lopez <juanpa9951@gmail.com>
2024-04-22 16:25:16 +0200

    new version

--- dxf_spline_multiple_read.py
+++ dxf_spline_multiple_read.py
@@ -85,9 +85,106 @@ def read_and_plot_layerv0(file_path):
 
     print('total layers = ',layer_qty)
 
+def read_and_plot_layerv1(file_path):
+    import ezdxf
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from pptx import Presentation
+    from pptx.util import Inches
+
+
+    ##............READING THE DXF
+    doc = ezdxf.readfile(file_path)
+
+    # POWERPOINT SLIDE
+    prs = Presentation()
+    slide_layout = prs.slide_layouts[6]  # Blank slide layout
+    Left_centering=1.2   #in inches
+
+    # Extract entities (splines)
+    msp = doc.modelspace()
+    splines = msp.query('SPLINE')
+    i=0
+    x=[]
+    y=[]
+    # Iterate over splines
+    for spline in splines:
+        # Check if the spline is a BSpline
+        if spline.dxftype() == 'SPLINE':  # here we look for entity type SPLINE
+            # Get control points of the spline
+            control_points = spline._control_points
+
+            # Print control points
+            for point in control_points:
+                i=i+1
+                print("Vertice:", point,'vertice number', i)
+                x.append(round(point[0],8)) # Here we extract the x-coordenates of the vertices of the spline
+                y.append(round(point[1],8)) # Here we extract the y-coordenates of the vertices of the spline
+
+    #### TRANSFORMATION FOR CENTERING
+    ####.....1st bring to the origin all the layers   OPTIONAL
+    x=[i - min(x)*1 for i in x]
+    y=[i - min(y)*1 for i in y]
+    Axis_Limit=20
+    #####...................2nd scale reduction  OPTIONAL...........................
+    x = [i / 100 for i in x]
+    y = [i / 100 for i in y]
+    ####...................3rd scale reduction...........................
+    fx=20/62
+    fy=20/60
+    Axis_Limit_x = Axis_Limit
+    Axis_Limit_y = Axis_Limit
+
+    x = [i*fx for i in x]
+    y = [i*fy for i in y]
+
+
+    x_init=x[0]
+    y_init=y[0]
+    z=0
+    layer_qty=0
+    check=0
+    for i in range(0,len(x)):    # loop through all the vertices of the splines found
+        #print(i)
+        if x[i]==x_init and y[i]==y_init:   # layer detection condition is when there is a closed loop, initial and final points are equal
+            check=check+1 # the initial point of the loop
+            if check==2: # the final point of the loop
+                x_layer=x[z:i+ 1]
+                y_layer = y[z:i + 1]
+                z=i+1
+                layer_qty=layer_qty+1
+                check=0
+                if i<(len(x)-1):
+                 x_init = x[i+1]
+                 y_init = y[i+1]
+                print('Layer ', layer_qty)
+                print('X_coordenates ', x_layer)
+                print('y_coordenates ', y_layer)
+
+                # Plotting the polygon
+                plt.figure(figsize=(7, 7))
+                plt.plot(x_layer, y_layer, color='red', linestyle='-', linewidth=1, marker='o', markersize=3) # 'bo-' means blue circles connected by lines
+                plt.fill(x_layer, y_layer, alpha=1)  # Fill the polygon
+                title='Layer '+str(layer_qty)
+                plt.title(title)
+                plt.grid(True, linestyle='--', linewidth=0.5)  # Set linewidth to 1.5
+                plt.xlim(0, Axis_Limit_x)  # Set x-axis limit from 0 to 4
+                plt.ylim(0, Axis_Limit_y)  # Set y-axis limit from 0 to 4
+                plt.savefig('Layers.png')
+                # add slide
+                slide = prs.slides.add_slide(slide_layout)
+                img_path = 'Layers.png'
+                left = Inches(Left_centering)  # Adjust position as needed in icnhes
+                top = Inches(0)  # Adjust position as needed in inches
+                slide.shapes.add_picture(img_path, left, top)
+    # Save the PowerPoint presentation
+    prs.save('Layers.pptx')
+
+
+    print('total layers = ',layer_qty)
 
 file_path='36_LRU12.dxf'
-read_and_plot_layerv0(file_path)
+read_and_plot_layerv1(file_path)
 
 
 
