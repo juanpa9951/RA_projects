0d4efc37bbedcb1db031413476602446da5a8b7a
Juan Pablo Lopez <juanpa9951@gmail.com>
2024-05-09 09:24:30 +0200

    new version

--- dxf_exploder.py
+++ dxf_exploder.py
@@ -37,31 +37,44 @@ def explode_insertV0(file_path):
     # Save the modified DXF file
     doc.saveas(file_path)
 
-def explode_insertV1(file_path,explode_times):
-    # Load the DXF file
-    doc = ezdxf.readfile(file_path)
+def explode_insertV1(Origin_path,Destination_path,explode_times):
 
-    # Get the modelspace
-    msp = doc.modelspace()
+    import ezdxf
+    import os
+    import pandas as pd
+    #.................................................................
 
-    # First explode scan
-    for i in range(0,explode_times):
-        for entity in msp:
-            # Check if the entity is an 'INSERT'
-            if entity.dxftype() == 'INSERT':
-                # Explode the 'INSERT' entity
-                entity.explode()
+    DXF_Names = os.listdir(Origin_path)  # this extracts the names of the files inside source folder
+    for k1 in DXF_Names:  # this is for eliminating the files that are not photos
+        if k1.endswith("dxf") == False:  # dxf
+            Path2Remove = f"{Origin_path}\{k1}"
+            os.remove(Path2Remove)
+    DXF_Names = os.listdir(Origin_path)  # this is to update the names after deleting non dxf files
+    print(" TOTAL OF ", len(DXF_Names), " DXF UPLOADED")
+    for name in DXF_Names:
+            dxf_file = f"{Origin_path}\{name}"
+            Name1 = name[-17:-4]  # file identification
+            # Load the DXF file
+            doc = ezdxf.readfile(dxf_file)
+            # Get the modelspace
+            msp = doc.modelspace()
+            # First explode scan
+            for i in range(0,explode_times):
+                for entity in msp:
+                    # Check if the entity is an 'INSERT'
+                    if entity.dxftype() == 'INSERT':
+                        # Explode the 'INSERT' entity
+                        entity.explode()
 
-        # Save the modified DXF file
-        doc.saveas(file_path)
-        print('file exploded ',i+1,' times')
-
-
-# Specify the file path of the DXF file
-file_path = 'V236_WW_TRU05.dxf'
-explode_times=1   # how many times you want to explode
-
+            # Save the modified DXF file
+            Name2='Exploded_'+Name1+'.dxf'
+            out_Name= f"{Destination_path}\{name}"
+            doc.saveas(out_Name)
+            #print('file exploded ',i+1,' times')
 
 # explode_insertV0(file_path)
 
-explode_insertV1(file_path,explode_times)
+Origin_path=r'C:\Users\Juan Pablo Lopez\OneDrive - Rewair A S\Desktop\PFILES\Python_versions\Origin'
+Destination_path=r'C:\Users\Juan Pablo Lopez\OneDrive - Rewair A S\Desktop\PFILES\Python_versions\Exploded'
+explode_times=5   # how many times you want to explode
+explode_insertV1(Origin_path,Destination_path,explode_times)
---
+++ projection_03.dwl
@@ -0,0 +1,3 @@
+Juan Pablo Lopez
+RAES034 
+Thursday, 9 May 2024  09.17.30
