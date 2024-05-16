23892d5b7939e9c7a4c2d7d77961378a744fa0c1
Juan Pablo Lopez <juanpa9951@gmail.com>
2024-05-16 11:27:17 +0200

    new version

--- dxf_generator.py
+++ dxf_generator.py
@@ -3569,6 +3569,196 @@ def Transform_DXF_V20(Origin_path, Destination_path, excel_order):   # TAGGING V
     print('Error NO layers: ', Error_files)
     print('Error missing layers: ', Error_capas)    #
 
+def Transform_DXF_V21(Origin_path,Destination_path,excel_order):  #### V21 TAGGING ERROR CORRECTION with full layers name in disorder
+    import ezdxf
+    import os
+    import pandas as pd
+
+    ## .... excel data needed for colors and materials................
+    ## excel_order = r'C:\Users\Juan Pablo Lopez\OneDrive - Rewair A S\Desktop\PFILES\Python_versions\DXF_order.xlsx'
+    material_table = pd.read_excel(excel_order, sheet_name='Sheet2', header=0)
+    #.................................................................
+
+    DXF_Names = os.listdir(Origin_path)  # this extracts the names of the files inside source folder
+    for k1 in DXF_Names:  # this is for eliminating the files that are not photos
+        if k1.endswith("dxf") == False:  # dxf
+            Path2Remove = f"{Origin_path}\{k1}"
+            os.remove(Path2Remove)
+    DXF_Names = os.listdir(Origin_path)  # this is to update the names after deleting non dxf files
+    print(" TOTAL OF ", len(DXF_Names), " DXF UPLOADED")
+    file_counter=0
+    Error_files=[]
+    Error_capas=[]
+    for name in DXF_Names:
+            dxf_file = f"{Origin_path}\{name}"
+            Name1 = name[-17:-4]  # file identification
+            Order = name[:2]  # order of files given
+            try:
+                h=int(Order)
+            except:
+                print('ERROR: ',Name1,' no tiene orden de archivo')
+                break
+            ##............READING THE INPUT DXF
+            doc = ezdxf.readfile(dxf_file)
+            ##...EXTRACT THE FIRST REFERENCE BLOCK ( FIRST TEXTBOX IT FINDS) TO GET THE LAYERS NAMES
+            block_refs = doc.modelspace().query('INSERT')
+            Layer_Names=[]
+            for block in block_refs:
+                Layer_Name_i=block.dxf.name  # TIENE QUE HABER UN TEXTBOX EN EL DXF ORIGINAL SINO ESTO DA ERROR
+                position=Layer_Name_i.find('V236')   # find if the text has V236 in it
+                if position!=-1:   # if V236 is not present then position returns -1, here we filter that
+                    Layer_Name_i=Layer_Name_i[position:position+18]
+                    Layer_Names.append(Layer_Name_i)
+            # Create a new OUTPUT DXF document
+            doc2 = ezdxf.new(dxfversion='R2010')  # 10  13 18
+            msp2 = doc2.modelspace()
+
+            qty_layers=len(Layer_Names)
+            Generic_Layer=Layer_Names[0]
+            doc2.layers.new(name=Generic_Layer)
+            #doc2.layers.get(Generic_Layer).set_color(30)  # 30 es como cafe-naranja
+            #msp2.add_lwpolyline(vertices).dxf.layer='FRAME'
+
+
+            # Extract entities (splines)
+            msp = doc.modelspace()
+            splines = msp.query('SPLINE')
+            polylines= msp.query('POLYLINE')
+            lines = msp.query('LINE')
+
+            #### CHECK IF THE FILE IS DECOMPOSED FIRST
+            if len(splines)==0 and len(polylines)==0:
+                print('ERROR: dxf', name, 'is not decomposed')
+                break
+            ####............................................
+            i3 = 0 # JUST FOR DEBUGGING
+            # color=[]
+
+            ####...........CREATE THE TAGGING LAYER...........
+            doc2.layers.new(name='TAGGING')
+            doc2.layers.get('TAGGING').set_color(256)  # assign the color of the TAGGING
+            doc2.layers.new(name='DATUM LINE')
+            doc2.layers.get('DATUM LINE').set_color(141)  # assign the color of the TAGGING
+            ###..................................................
+
+            # Iterate over splines
+            counter=0
+            for spline in splines:
+                # Check if the spline is a BSpline
+
+                if spline.dxftype() == 'SPLINE':  # here we look for entity type SPLINE
+
+                        control_points = spline._control_points
+                        x = []
+                        y = []
+                        color = []
+                        # EXTRACT control points, i.e  VERTICES OF THE SPLINES
+                        for point in control_points:
+                            i3 = i3 + 1    # JUST FOR DEBUGGING
+                            # print("Vertice:", point, 'vertice number', i)
+                            x.append(round(point[0], 6))  # Here we extract the x-coordenates of the vertices of the spline rounded to 8 decimals
+                            y.append(round(point[1], 6))  # Here we extract the y-coordenates of the vertices of the spline  8 rounded to 8 decimals
+                            color.append(spline.dxf.color)
+
+                        Tuples = []
+                        for i in range(0, len(x)):
+                            tuple_i = (x[i], y[i])
+                            Tuples.append(tuple_i)
+                        polyline_new = doc2.modelspace().add_lwpolyline(Tuples)  # THIS IS REALLY A POLYLINE # Adds a polyline to the modelspace
+                        polyline_new.dxf.color=spline.dxf.color #assign the color of the original spline
+
+                        if counter<qty_layers:
+                          polyline_new.dxf.layer = Layer_Names[counter]
+                          counter=counter+1
+                        else:
+                          polyline_new.dxf.layer = Generic_Layer
+
+            counter = 0
+            for spline in polylines:
+                # Check if the spline is a BSpline
+
+                if spline.dxftype() == 'POLYLINE':  # here we look for entity type POLYLINE
+
+                    control_points = spline.points()
+                    x = []
+                    y = []
+                    color = []
+
+                    # EXTRACT control points, i.e  VERTICES OF THE SPLINES
+                    for point in control_points:
+                        i3 = i3 + 1  # JUST FOR DEBUGGING
+                        # print("Vertice:", point, 'vertice number', i)
+                        x.append(round(point[0],6))  # Here we extract the x-coordenates of the vertices of the spline rounded to 8 decimals
+                        y.append(round(point[1],6))  # Here we extract the y-coordenates of the vertices of the spline  8 rounded to 8 decimals
+                        color.append(spline.dxf.color)
+
+                    Tuples = []
+                    for i in range(0, len(x)):
+                        tuple_i = (x[i], y[i])
+                        Tuples.append(tuple_i)
+                    polyline_new = doc2.modelspace().add_lwpolyline(Tuples)  # THIS IS REALLY A POLYLINE # Adds a polyline to the modelspace
+                    polyline_new.dxf.color = spline.dxf.color  # assign the color of the original spline
+
+                    if counter < qty_layers:
+                        polyline_new.dxf.layer = Layer_Names[counter]
+                        counter = counter + 1
+                    else:
+                        polyline_new.dxf.layer = Generic_Layer
+
+            counter = 0
+            for spline in lines:
+                # Check if the spline is a BSpline
+                if spline.dxftype() == 'LINE':  # here we look for entity type LINE
+
+                     if spline.dxf.color!=5 and spline.dxf.color!=150:   # CHECK IF ITS NOT A REFERENCE LINE (BLUE LINE)
+                        # Get control points of the spline
+                         control_points = [spline.dxf.start, spline.dxf.end]
+                         x = []
+                         y = []
+                         color = []
+                         for point in control_points:
+                             x.append(round(point[0], 6))  # Here we extract the x-coordenates of the vertices of the spline rounded to 8 decimals
+                             y.append(round(point[1], 6))  # Here we extract the y-coordenates of the vertices of the spline  8 rounded to 8 decimals
+                             color.append(spline.dxf.color)
+                         Tuples = []
+                         for i in range(0, len(x)):
+                             tuple_i = (x[i], y[i])
+                             Tuples.append(tuple_i)
+                         polyline_new = doc2.modelspace().add_lwpolyline(Tuples)  # THIS IS REALLY A POLYLINE # Adds a polyline to the modelspace
+                         polyline_new.dxf.color = spline.dxf.color  # assign the color of the original spline
+                         if spline.dxf.color==256 or spline.dxf.color==255:
+                             polyline_new.dxf.layer = 'TAGGING'
+                         elif spline.dxf.color==141:
+                             polyline_new.dxf.layer = 'DATUM LINE'
+                         elif counter < qty_layers:
+                             polyline_new.dxf.layer = Layer_Names[counter]
+                             counter = counter + 1
+                         else:
+                             polyline_new.dxf.layer = Generic_Layer
+
+
+
+            # Define the FRAME vertices and add them to the drawing
+            vertices = [(0, 0), (10000, 0), (10000, 2500), (0, 2500), (0, 0)]
+            doc2.layers.new(name='FRAME')
+            doc2.layers.get('FRAME').set_color(30)  # 30 es como cafe-naranja
+            msp2.add_lwpolyline(vertices).dxf.layer='FRAME'
+
+            # Save the DXF file
+            Name2='Nombrar_capas_'+str(Order)+'_'+Name1+'.dxf'
+            out_Name= f"{Destination_path}\{Name2}"
+            doc2.saveas(out_Name)
+            #########...............................................................................................................
+            file_counter=file_counter+1
+            print('File number ',file_counter," of ", len(DXF_Names), ' File Name ', Name2)
+
+
+    # print('Error NO layers: ', Error_files)
+    # print('Error missing layers: ', Error_capas)
+
+
+
+
 Origin_path=r'C:\Users\Juan Pablo Lopez\OneDrive - Rewair A S\Desktop\PFILES\Python_versions\Origin'
 Destination_path=r'C:\Users\Juan Pablo Lopez\OneDrive - Rewair A S\Desktop\PFILES\Python_versions\Destiny'
 excel_order = r'C:\Users\Juan Pablo Lopez\OneDrive - Rewair A S\Desktop\PFILES\Python_versions\DXF_order.xlsx'
@@ -3591,7 +3781,7 @@ excel_order = r'C:\Users\Juan Pablo Lopez\OneDrive - Rewair A S\Desktop\PFILES\P
 ####  V20  TAGGING VERSION MULTIPLE MIDLINES (DATUMS)
 
 
-Transform_DXF_V17(Origin_path,Destination_path,excel_order)
+Transform_DXF_V21(Origin_path,Destination_path,excel_order)
 
 
 
