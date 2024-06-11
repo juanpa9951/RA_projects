68cc806540d3af8d817b2538d14f7841b49baacb
Juan Pablo Lopez <juanpa9951@gmail.com>
2024-06-11 15:49:16 +0200

    new version

--- dxf_masterPattern.py
+++ dxf_masterPattern.py
@@ -3,6 +3,7 @@ def master_pattern(image_size,Left_centering,Top_centering,Axis_Limit,background
     import matplotlib.pyplot as plt
     from pptx import Presentation
     from pptx.util import Inches
+    import numpy as np
 
 
     # POWERPOINT SLIDE
@@ -16,40 +17,50 @@ def master_pattern(image_size,Left_centering,Top_centering,Axis_Limit,background
     x_base=500
     y_base=200
 
-    for i in range(1,15):
-                x_layer=[0,x_base*i]
-                y_layer=[y_base,y_base]
-                Layer_Name_plot='pattern'
+    for k in range(1,13):   # 12
+            for i in range(1,17):  # 17
+                        x_lenght=x_base*i
+                        y_lenght=y_base*k
+                        x_layer=[0,x_lenght]
+                        y_layer=[y_lenght,y_lenght]
+                        Layer_Name_plot='X='+str(x_lenght)+" Y="+str(y_lenght)
 
-                # Plotting the polygon
-                plt.figure(figsize=(image_size, image_size))
-                plt.plot(x_layer, y_layer,color='blue', linestyle='-', linewidth=0.3, marker='x',markersize=0.3) # 'bo-' means blue circles connected by lines
-                title='pattern'
-                plt.title(title)
-                plt.xlim(0, Axis_Limit_x)  # Set x-axis limit from 0 to 4
-                plt.ylim(0, Axis_Limit_y)  # Set y-axis limit from 0 to 4
-                plt.style.use('dark_background')
-                ax = plt.gca()
-                ax.set_facecolor(background_color)
-                ax.spines['top'].set_color(layer_color)
-                ax.spines['bottom'].set_color(layer_color)
-                ax.spines['right'].set_color(layer_color)
-                ax.spines['left'].set_color(layer_color)
-                ax.tick_params(axis='x', colors=layer_color)
-                ax.tick_params(axis='y', colors=layer_color)
-                ax.set_title(ax.get_title(), color=layer_color)  # Update title with green color
-                plt.text(2000, 50, Layer_Name_plot, fontsize=15, color='red')
-                plt.savefig('Layers.png')
-                # add slide
-                slide = prs.slides.add_slide(slide_layout)
-                img_path = 'Layers.png'
-                left = Inches(Left_centering)  # Adjust position as needed in icnhes
-                top = Inches(Top_centering)  # Adjust position as needed in inches
-                slide.shapes.add_picture(img_path, left, top)
-                if close_image==1:
-                 plt.close('all')
+                        # Plotting the polygon
+                        plt.figure(figsize=(image_size, image_size))
+                        plt.plot(x_layer, y_layer,color='blue', linestyle='-', linewidth=0.3, marker='x',markersize=0.3) # 'bo-' means blue circles connected by lines
+                        title='pattern'
+                        plt.title(title)
 
-                # prs.save('Layers.pptx')
+                        x_ticks = np.arange(0, 8500, 500)  # From 0 to 6 with a step of 0.5
+                        y_ticks = np.arange(0, 8500, 8500)  # From 0 to 12 with a step of 1
+                        plt.xticks(x_ticks)
+                        plt.yticks(y_ticks)
+
+                        plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.7)
+                        plt.xlim(0, Axis_Limit_x)  # Set x-axis limit from 0 to 4
+                        plt.ylim(0, Axis_Limit_y)  # Set y-axis limit from 0 to 4
+                        plt.style.use('dark_background')
+                        ax = plt.gca()
+                        ax.set_facecolor(background_color)
+                        ax.spines['top'].set_color(layer_color)
+                        ax.spines['bottom'].set_color(layer_color)
+                        ax.spines['right'].set_color(layer_color)
+                        ax.spines['left'].set_color(layer_color)
+                        ax.tick_params(axis='x', colors=layer_color)
+                        ax.tick_params(axis='y', colors=layer_color)
+                        ax.set_title(ax.get_title(), color=layer_color)  # Update title with green color
+                        plt.text(2000, 50, Layer_Name_plot, fontsize=15, color='red')
+                        plt.savefig('Layers.png')
+                        # add slide
+                        slide = prs.slides.add_slide(slide_layout)
+                        img_path = 'Layers.png'
+                        left = Inches(Left_centering)  # Adjust position as needed in icnhes
+                        top = Inches(Top_centering)  # Adjust position as needed in inches
+                        slide.shapes.add_picture(img_path, left, top)
+                        if close_image==1:
+                         plt.close('all')
+
+                        # prs.save('Layers.pptx')
 
     prs.save('Master_Pattern.pptx')
 
