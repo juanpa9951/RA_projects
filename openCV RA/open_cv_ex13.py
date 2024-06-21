5cdbbbfdd1486539b72aece04b8cb15e72b9f5a2
Juan Pablo Lopez <juanpa9951@gmail.com>
2024-06-21 11:22:50 +0200

    new version

--- openCV RA/open_cv_ex13.py
+++ openCV RA/open_cv_ex13.py
@@ -1,41 +1,151 @@
-# THIS CODE DISPLAYS LIVE PIXEL POSITIONS ACROSS THE IMAGE
+# feeder camera testing site
+def run_camera():  # LIVE PIXEL POSITIONS ACROSS THE IMAGE
+    import os
+    os.chdir(r'C:\Users\Juan Pablo Lopez\PycharmProjects\ProjectJP\openCV RA')
 
-import os
-os.chdir(r'C:\Users\Juan Pablo Lopez\PycharmProjects\ProjectJP\openCV RA')
-
-import cv2
-import numpy as np
-
-
-camera_url=0
-camera_url_der="rtsp://LP008:LP008ASM@192.168.2.82:554/stream1"
-camera_url_izq="rtsp://LP009:LP009ASM@192.168.2.84:554/stream1"
-cap = cv2.VideoCapture(camera_url_der)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # 1280
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # 720
+    import cv2
+    import numpy as np
 
 
-while True:
-    _, img = cap.read()
+    camera_url=0
+    camera_url_der="rtsp://LP008:LP008ASM@192.168.2.82:554/stream1"
+    camera_url_izq="rtsp://LP009:LP009ASM@192.168.2.84:554/stream1"
+    cap = cv2.VideoCapture(camera_url_der)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # 1280
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # 720
+
+
+    while True:
+        _, img = cap.read()
+        # hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV_FULL) # HSV ALTERNATIVE
+        hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)       # GRAYSCALE ALTERNATIVE
+        height, width, ch = img.shape
+        # print(height,width)
+
+
+        for x in range(0,width,50):
+         for y in range(0,height,30):  # 756 rows   1344 columns
+          font_type=cv2.FONT_HERSHEY_SIMPLEX
+          Text="o"
+          cv2.putText(img,Text,(x,y),font_type,0.1,(100,200,0),1)  # draw text
+          Text2 = '(' + str(y) + ',' + str(x) + ')'
+          cv2.putText(img, Text2, (x+3, y), font_type, 0.3, (100, 200, 0), 1)  # draw text
+
+        cv2.imshow("Image", img)
+        key = cv2.waitKey(1)
+        if key == 27:  # ESCAPE KEY to kill the camera
+         break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def run_camera2(): # LIVE PIXEL POSITIONS ACROSS THE IMAGE WITH DIFFERENT SPACING (MESH)
+    import os
+    os.chdir(r'C:\Users\Juan Pablo Lopez\PycharmProjects\ProjectJP\openCV RA')
+
+    import cv2
+    import numpy as np
+
+
+    camera_url=0
+    camera_url_der="rtsp://LP008:LP008ASM@192.168.2.82:554/stream1"
+    camera_url_izq="rtsp://LP009:LP009ASM@192.168.2.84:554/stream1"
+    cap = cv2.VideoCapture(camera_url_der)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # 1280
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # 720
+
+
+    while True:
+        _, img = cap.read()
+        # hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV_FULL) # HSV ALTERNATIVE
+        hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)       # GRAYSCALE ALTERNATIVE
+        height, width, ch = img.shape
+        # print(height,width)
+
+
+        for x in range(0,width,70):
+         for y in range(0,height,50):  # 756 rows   1344 columns
+          font_type=cv2.FONT_HERSHEY_SIMPLEX
+          Text="o"
+          cv2.putText(img,Text,(x,y),font_type,0.1,(100,200,0),1)  # draw text
+          Text2 = '(' + str(y) + ',' + str(x) + ')'
+          cv2.putText(img, Text2, (x+3, y), font_type, 0.3, (100, 200, 0), 1)  # draw text
+
+        cv2.imshow("Image", img)
+        key = cv2.waitKey(1)
+        if key == 27:  # ESCAPE KEY to kill the camera
+         break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def run_camera3():   ## SINGLE PIXEL LIVE POSITION ACROSS THE IMAGE
+    import os
+    os.chdir(r'C:\Users\Juan Pablo Lopez\PycharmProjects\ProjectJP\openCV RA')
+
+    import cv2
+    import numpy as np
+
+
+    camera_url=0
+    camera_url_der="rtsp://LP008:LP008ASM@192.168.2.82:554/stream1"
+    camera_url_izq="rtsp://LP009:LP009ASM@192.168.2.84:554/stream1"
+    cap = cv2.VideoCapture(camera_url_der)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # 1280
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # 720
+
+
+    while True:
+        _, img = cap.read()
+        # hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV_FULL) # HSV ALTERNATIVE
+        hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)       # GRAYSCALE ALTERNATIVE
+        height, width, ch = img.shape
+        print(height,width)
+
+        x=1730   #### pixel row
+        y=800   #### pixel column
+        font_type=cv2.FONT_HERSHEY_SIMPLEX
+        Text="o"
+        cv2.putText(img,Text,(x,y),font_type,0.1,(100,200,0),1)  # draw text
+        Text2 = '(' + str(y) + ',' + str(x) + ')'
+        cv2.putText(img, Text2, (x+3, y), font_type, 0.3, (100, 200, 0), 1)  # draw text
+
+        cv2.imshow("Image", img)
+        key = cv2.waitKey(1)
+        if key == 27:  # ESCAPE KEY to kill the camera
+         break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def run_camera4():  ### SINGLE PIXEL ACROSS IMAGE  (not live)
+    import os
+    os.chdir(r'C:\Users\Juan Pablo Lopez\PycharmProjects\ProjectJP\openCV RA')
+
+    import cv2
+    import numpy as np
+
+    path = 'Feeder1.png'
+    img = cv2.imread(path)  # standard BGR format each from 0-255
+
     # hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV_FULL) # HSV ALTERNATIVE
     hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)       # GRAYSCALE ALTERNATIVE
     height, width, ch = img.shape
+    print(height,width)
 
-
-    for x in range(0,width,50):
-     for y in range(0,height,30):  # 756 rows   1344 columns
-      Text='( '+str(y)+','+str(x)+' )'
-      Text="o"
-      cv2.putText(img,Text,(x,y),cv2.FONT_HERSHEY_PLAIN,0.5,(100,200,0),1)  # draw text
-
+    x=1730   #### pixel row
+    y=800   #### pixel column
+    font_type=cv2.FONT_HERSHEY_SIMPLEX
+    Text="o"
+    cv2.putText(img,Text,(x,y),font_type,0.1,(100,200,0),1)  # draw text
+    Text2 = '(' + str(y) + ',' + str(x) + ')'
+    cv2.putText(img, Text2, (x+3, y), font_type, 0.3, (100, 200, 0), 1)  # draw text
 
     cv2.imshow("Image", img)
-    key = cv2.waitKey(1)
-    if key == 27:  # ESCAPE KEY to kill the camera
-     break
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cap.release()
-cv2.destroyAllWindows()
+run_camera3()
 
 
 #  PIXELES VAN POR FILAS Y COLUMNAS, LO MISMO Q TIRA EL .SHAPE, EL IMG O EL HSV O EL GRAY TIRAN RESULTADO EN FILAS X COLUMNAS, PERO COORDENADAS ES INVERTIDO (COLUMNA-FILA) TOD O LO QUE SEA DIBUJAR SOBRE LA IMAGEN SERA EN COORDENADAS, PRIMERO LA COLUMNA Y LUEGO LA FILA
