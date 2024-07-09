75f0ffc2a3587481069bb6c487f76dae5de16110
Juan Pablo Lopez <juanpa9951@gmail.com>
2024-07-09 09:30:47 +0200

    new version

--- openCV RA/open_cv_ex22.py
+++ openCV RA/open_cv_ex22.py
@@ -1,3 +1,85 @@
+def euclidean_distance(point1, point2):
+    import math
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+def find_closest_tupleV2(tuples_list_real,tuples_list_autocad, input_tuple):
+    #### same as V1 but only 1 output
+    closest_tuple_low = None
+    closest_tuple_high = None
+    idx_low=None
+    idx_high=None
+
+    smallest_distance = float('inf')  # Initialize with a large number
+    i=0
+    for tup in tuples_list_real:
+        distance = euclidean_distance(tup, input_tuple)
+        if distance < smallest_distance and tup[0]<input_tuple[0] and tup[1]<input_tuple[1]:
+            smallest_distance = distance
+            closest_tuple_low = tup
+            idx_low=i
+        i=i+1
+
+    smallest_distance = float('inf')  # Initialize with a large number
+    i=0
+    for tup in tuples_list_real:
+        distance = euclidean_distance(tup, input_tuple)
+        if distance < smallest_distance and tup[0]>input_tuple[0] and tup[1]>input_tuple[1]:
+            smallest_distance = distance
+            closest_tuple_high = tup
+            idx_high=i
+        i=i+1
+
+    ### interpolate X value from real to autocad
+    target_value=input_tuple[0]
+    a_prev = tuples_list_real[idx_low][0]
+    a_next = tuples_list_real[idx_high][0]
+    b_prev = tuples_list_autocad[idx_low][0]
+    b_next = tuples_list_autocad[idx_high][0]
+    # Perform linear interpolation
+    interpolated_value = b_prev + ((target_value - a_prev) / (a_next - a_prev)) * (b_next - b_prev)
+    x_interp=interpolated_value
+
+
+    ### interpolate Y value from real to autocad
+    target_value =input_tuple[1]
+    a_prev = tuples_list_real[idx_low][1]
+    a_next = tuples_list_real[idx_high][1]
+    b_prev = tuples_list_autocad[idx_low][1]
+    b_next = tuples_list_autocad[idx_high][1]
+    # Perform linear interpolation
+    interpolated_value = b_prev + ((target_value - a_prev) / (a_next - a_prev)) * (b_next - b_prev)
+    y_interp=interpolated_value
+
+    xy_tup=(x_interp,y_interp)
+
+
+    return xy_tup,idx_low,idx_high,closest_tuple_low,closest_tuple_high
+
+# .....LOAD THE TABLE CALIBRATION DATA.......................................................................................................
+import pandas as pd
+excel_table_calib = r'C:\Users\Juan Pablo Lopez\OneDrive - Rewair A S\Documents\Camaras\feeder_right.xlsx'
+Surface_map = pd.read_excel(excel_table_calib, sheet_name='right_t1', header=0)
+tuples_list_real=[]
+tuples_list_pixel=[]
+for i in range(0,len(Surface_map)):
+    x_tup_pixel = Surface_map['X_pixel'][i]
+    y_tup_pixel = Surface_map['Y_pixel'][i]
+    tuples_list_pixel.append((x_tup_pixel,y_tup_pixel))
+    x_tup_real = Surface_map['X_real'][i]
+    y_tup_real = Surface_map['Y_real'][i]
+    tuples_list_real.append((x_tup_real,y_tup_real))
+def distance_real(col1,col2,row1,row2):
+    import math
+    start_point_pixel = (col1, row1)
+    start_point_real, idx_low1, idx_high1, tup_low1, tup_high1 = find_closest_tupleV2(tuples_list_pixel,tuples_list_real,start_point_pixel)
+    end_point_pixel = (col2, row2)
+    end_point_real, idx_low2, idx_high2, tup_low2, tup_high2 = find_closest_tupleV2(tuples_list_pixel,tuples_list_real,end_point_pixel)
+    distance_X=round(abs(end_point_real[0]-start_point_real[0]),2)
+    distance_Y=round(abs(end_point_real[1]-start_point_real[1]),2)
+    hyp=round(math.sqrt(distance_X**2+distance_Y**2),2)
+    return distance_X, distance_Y,hyp
+
+
+
 import cv2
 
 # Global variables to store the mouse position and clicked points
@@ -112,10 +194,19 @@ def main():
         x_offset, y_offset = 10, 10  # Position of the zoomed frame in the corner
         frame[y_offset:y_offset + zoomed_frame_height, x_offset:x_offset + zoomed_frame_width] = zoomed_frame_small
 
-        # Draw lines between clicked points
+
         # Draw the lines and coordinates on the frame
         for line in lines:
             cv2.line(frame, line[0], line[1], (0, 255, 0), 2)
+        for i, line in enumerate(lines):
+            if i < 2:  # Only display the coordinates of the first two lines
+                col1 = line[0][0]
+                col2 = line[1][0]
+                row1 = line[0][1]
+                row2 = line[1][1]
+                xt, yt, hyp = distance_real(col1, col2, row1, row2)
+                cv2.putText(frame, f"dist x= {xt}, dist y= {yt}, hyp= {hyp}", (1000, 30 + i * 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
 
         # Display the combined frame
         cv2.imshow('Video Feed', frame)
